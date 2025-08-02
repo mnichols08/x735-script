c06b2c39287dc06e40511fd0c7d9d0827c9067b3
Mikey Nichols <mnix@journeytocode.io>
2025-08-02 14:50:12 -0400

    adds folder for x735 executables

--- install-sss.sh
+++ install-sss.sh
@@ -3,7 +3,7 @@
 # Install soft shutdown script (sss)
 echo "Start installing the soft shutdown script (sss)..."
 
-sudo cp -f ./xSoft.sh                /usr/local/bin/
+sudo cp -f ./xSoft.sh                /usr/local/bin/x735
 
 # echo "Create a alias 'x735off' command to execute the software shutdown"
 # echo 'alias x735off="sudo /usr/local/bin/xSoft.sh 0 20"' >> ~/.bashrc
